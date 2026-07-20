36f04a167c716b52245cc7a87b78bfdcfb8c4cc6
SiddhanthTA <tasiddhanth@gmail.com>
2026-07-20 11:26:13 +0530

    Hands On - 2

---
+++ React/studentapp/src/Components/About.js
@@ -0,0 +1,5 @@
+function About() {
+    return <h2>Welcome to the About page of Student Management Portal</h2>;
+}
+
+export default About;
---
+++ React/studentapp/src/Components/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+    return <h2>Welcome to the Contact page of Student Management Portal</h2>;
+}
+
+export default Contact;
